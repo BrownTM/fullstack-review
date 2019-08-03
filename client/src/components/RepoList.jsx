482fbf6e885a4a7887d8c540996aa50269d6b4d7
Therese Brown <browntm96@gmail.com>
2019-08-02 20:06:14 -0700

    Complete displaying top 25 repos

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const RepoList = (props) => (
   <div>
     <h4> Repo List Component </h4>
-    There are {props.repos.length} repos.
+    <ol>
+      {props.repos.map((repo, i) => {
+        return <li key={i}>{repo.repoName} - {repo.username}</li>
+      })}
+    </ol>
+    {/* There are {props.repos.length} repos. */}
   </div>
 )
 
